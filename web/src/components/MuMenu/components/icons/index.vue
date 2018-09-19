dd87863d046719d278d7b8c1e18a0dd11d543a2f
woniu <745428482@qq.com>
2018-09-19 20:18:14 +0800

    跳转小图标组件

--- web/src/components/MuMenu/components/icons/index.vue
+++ web/src/components/MuMenu/components/icons/index.vue
@@ -1,18 +1,18 @@
 <template>
-    <div class="container">
-      <ul>
-        <li class="lightYellow"><a href="">松松商城</a></li>
-        <li class="pink"><a href="">淘宝小店</a></li>
-        <li class="blue"><a href="">松松软文</a></li>
-        <li class="green"><a href="">松松主机</a></li>
-      </ul>
-    </div>
+  <div class="container">
+    <ul>
+      <li class="lightYellow"><a href="">松松商城</a></li>
+      <li class="pink"><a href="">淘宝小店</a></li>
+      <li class="blue"><a href="">松松软文</a></li>
+      <li class="green"><a href="">松松主机</a></li>
+    </ul>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "icons"
-    }
+export default {
+  name: 'icons'
+}
 </script>
 
 <style scoped lang="less">
